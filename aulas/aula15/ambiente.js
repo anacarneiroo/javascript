cd6f7f2ac2de195970dc5d63306ab78156428c04
anacarneiroo <139649992+anacarneiroo@users.noreply.github.com>
2024-12-21 21:44:19 -0300

    aula 15

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,4 @@
+let num = [1]
+num.push(6)
+
+console.log(`Nosso vetor é o ${num}`)
---
+++ aulas/aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [8,1,7,4,2]
+
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(valores[pos])
+}
